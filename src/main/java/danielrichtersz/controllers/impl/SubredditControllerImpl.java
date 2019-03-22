da1394639111e0d5ac82624a0ee35cb2fd2c04d0
DanielRichtersz <digidi-daantje@hotmail.com>
2019-03-22 10:33:01 +0100

    Testing dependencies added, branching off

--- src/main/java/danielrichtersz/controllers/impl/SubredditControllerImpl.java
+++ src/main/java/danielrichtersz/controllers/impl/SubredditControllerImpl.java
@@ -13,9 +13,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
-import java.util.List;
-
 @Controller
 @RequestMapping("/api")
 @Api(value = "Subreddit Controller Implementation", description = "Operations pertaining to subreddits through Spring Boot REST API")
